b82994ed68ce0d05e4fd3acb895e3bf6cefca778
Kelvin-Ben <kevoben14@gmail.com>
2024-11-18 20:12:46 +0300

    Create a component to load all countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import CountryList from './components/CountryList'
+import LoadCountries from './components/LoadCountries'
 import './index.css'
 const App = () => {
   return (
     <div className='px-9'>
+      <LoadCountries />
       <CountryList />
     </div>
   )

b5ed9efe06e27cec36c9c6baaa2a281563047c18
Kelvin-Ben <kevoben14@gmail.com>
2024-11-18 20:13:26 +0300

    Pass state through recoil to the component

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,53 +1,15 @@
-const Country = () => {
+import { CountryInterface } from "../utils/recoilState";
+
+const Country = ({name, population, region, capital, flag}: CountryInterface) => {
   return (
     <>
     <div className="w-full shadow-lg border rounded-md bg-inherit dark:bg-inherit">
-      <img src="https://flagcdn.com/w320/af.png" className="w-full rounded-t-md" alt="a country sample" />
-      <h1 className="text-2xl font-bold py-2 px-4">Germany</h1>
+      <img src={flag} className="w-full rounded-t-md" alt={`${name} flag`} />
+      <h1 className="text-2xl font-bold py-2 px-4">{name}</h1>
       <div className="flex flex-col px-4 pb-6">
-        <p className="flex"><span className="font-semibold">Population:</span> <span className="ml-2">7,843,478</span> </p>
-        <p className="flex"><span className="font-semibold">Region:</span><span className="ml-2">Europe</span></p>
-        <p className="flex"><span className="font-semibold">Capital:</span> <span className="ml-2">Berlin</span></p>
-      </div>
-    </div>
-    {/* another card */}
-    <div className="w-full shadow-lg border rounded-md bg-inherit dark:bg-inherit">
-      <img src="https://flagcdn.com/w320/af.png" className="w-full rounded-t-md" alt="a country sample" />
-      <h1 className="text-2xl font-bold py-2 px-4">Germany</h1>
-      <div className="flex flex-col px-4 pb-6">
-        <p className="flex"><span className="font-semibold">Population:</span> <span className="ml-2">7,843,478</span> </p>
-        <p className="flex"><span className="font-semibold">Region:</span><span className="ml-2">Europe</span></p>
-        <p className="flex"><span className="font-semibold">Capital:</span> <span className="ml-2">Berlin</span></p>
-      </div>
-    </div>
-    {/*  */}
-    <div className="w-full shadow-lg border rounded-md bg-inherit dark:bg-inherit">
-      <img src="https://flagcdn.com/w320/af.png" className="w-full rounded-t-md" alt="a country sample" />
-      <h1 className="text-2xl font-bold py-2 px-4">Germany</h1>
-      <div className="flex flex-col px-4 pb-6">
-        <p className="flex"><span className="font-semibold">Population:</span> <span className="ml-2">7,843,478</span> </p>
-        <p className="flex"><span className="font-semibold">Region:</span><span className="ml-2">Europe</span></p>
-        <p className="flex"><span className="font-semibold">Capital:</span> <span className="ml-2">Berlin</span></p>
-      </div>
-    </div>
-    {/*  */}
-    <div className="w-full shadow-lg border rounded-md bg-inherit dark:bg-inherit">
-      <img src="https://flagcdn.com/w320/af.png" className="w-full rounded-t-md" alt="a country sample" />
-      <h1 className="text-2xl font-bold py-2 px-4">Germany</h1>
-      <div className="flex flex-col px-4 pb-6">
-        <p className="flex"><span className="font-semibold">Population:</span> <span className="ml-2">7,843,478</span> </p>
-        <p className="flex"><span className="font-semibold">Region:</span><span className="ml-2">Europe</span></p>
-        <p className="flex"><span className="font-semibold">Capital:</span> <span className="ml-2">Berlin</span></p>
-      </div>
-    </div>
-    {/*  */}
-    <div className="w-full shadow-lg border rounded-md bg-inherit dark:bg-inherit">
-      <img src="https://flagcdn.com/w320/af.png" className="w-full rounded-t-md" alt="a country sample" />
-      <h1 className="text-2xl font-bold py-2 px-4">Germany</h1>
-      <div className="flex flex-col px-4 pb-6">
-        <p className="flex"><span className="font-semibold">Population:</span> <span className="ml-2">7,843,478</span> </p>
-        <p className="flex"><span className="font-semibold">Region:</span><span className="ml-2">Europe</span></p>
-        <p className="flex"><span className="font-semibold">Capital:</span> <span className="ml-2">Berlin</span></p>
+        <p className="flex"><span className="font-semibold">Population:</span> <span className="ml-2">{population.toLocaleString()}</span> </p>
+        <p className="flex"><span className="font-semibold">Region:</span><span className="ml-2">{region}</span></p>
+        <p className="flex"><span className="font-semibold">Capital:</span> <span className="ml-2">{capital}</span></p>
       </div>
     </div>
     </>

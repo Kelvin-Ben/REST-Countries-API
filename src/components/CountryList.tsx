9e3baad34204845bd75400b7937313cca453c442
Kelvin-Ben <kevoben14@gmail.com>
2025-01-28 22:52:10 +0300

    This is a component rendering list of countries fetched

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,17 +1,17 @@
 import { useRecoilValue } from "recoil";
-import { countryDataState } from "../utils/recoilState";
+import { filteredCountrySelector } from "../utils/recoilState";
 import Country from "./Country";
 import { Link } from "react-router-dom";
-
 const CountryList = () => {
-  const countries = useRecoilValue(countryDataState);
+  const countries = useRecoilValue(filteredCountrySelector);
+
   return (
     <div className="flex flex-wrap sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-4">
-      {countries.map((country => (
+      {countries.map((country) => (
         <Link to={`/country/${country.numericCode}`} key={country.numericCode}>
-          <Country  {...country} />
+          <Country {...country} />
         </Link>
-      )))}
+      ))}
     </div>
   );
 };

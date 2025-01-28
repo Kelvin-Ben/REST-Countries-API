7c348e2d8e74db82c962926aee3456a352bf6d5e
Kelvin-Ben <kevoben14@gmail.com>
2025-01-28 22:56:28 +0300

    Create a component to facilitate the searching for a specific country.

--- src/components/SearchTerm.tsx
+++ src/components/SearchTerm.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react"
+import { useSetRecoilState } from "recoil";
+import { searchTermAtom } from "../utils/recoilState";
+import { useState } from "react";
 
 const SearchTerm = () => {
-  const [term, setTerm] = useState('');
-  console.log(term)
+  const setSearchTerm = useSetRecoilState(searchTermAtom);
+  const [inputValue, setInputValue] = useState("");
 
   function handleChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
-    const inputValue = e.currentTarget.value;
-    setTerm(inputValue)
+    const value = e.currentTarget.value;
+    setInputValue(value);
+
+    setSearchTerm(inputValue);
   }
+
   return (
     <div>
-      <input type="text" className="" value={term}  onChange={handleChangeInput} />
+      <input type="search" className="" onChange={handleChangeInput} />
     </div>
-  )
-}
+  );
+};
 export default SearchTerm;

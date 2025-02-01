317b03c83ad4fef4623e5d06b887b8e5a7c0ba12
Kelvin-Ben <kevoben14@gmail.com>
2025-02-01 15:36:22 +0300

    Improve component rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
 import CountryDetailPage from "./components/CountryDetailPage";
 import SearchTerm from "./components/SearchTerm";
 import CountryList from "./components/CountryList";
-import { useFetchCountries } from "./utils/hooks";
+import { useFetchCountries } from "./utils/fetchData";
 import ContinentSelector from "./components/ContinentSelector";
+import Header from "./components/Header";
+import "./index.css";
 
 const App = () => {
   useFetchCountries();
   return (
-    <>
-      <SearchTerm />
-      <ContinentSelector />
-      <Router>
-        <Routes>
-          <Route path="/" element={<CountryList />} />
-          <Route path="country/:numericCode" element={<CountryDetailPage />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <div className="min-h-[100vh] dark:bg-gray-900 dark:text-white">
+          <Header />
+          <Routes>
+              <Route path="/" element={
+                <div className="">
+                  <div className="flex justify-between px-[3rem] py-[1rem]">
+                    <SearchTerm />
+                    <ContinentSelector />
+                  </div>
+                  <CountryList />
+                </div>
+                } />
+              <Route path="country/:numericCode" element={<CountryDetailPage />} />
+          </Routes>
+      </div>
+        </Router>
   );
 };
 export default App;

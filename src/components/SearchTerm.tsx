5ea519181688c18154f701829d07863d3cfce42c
Kelvin-Ben <kevoben14@gmail.com>
2025-04-03 15:53:45 +0300

    Implement a searchTerm component for user to search for country

--- src/components/SearchTerm.tsx
+++ src/components/SearchTerm.tsx
@@ -1,7 +1,7 @@
 import { useRecoilState } from "recoil";
 import { searchTermAtom } from "../utils/recoilState";
 import { Search } from "lucide-react";
-import UseDebouncedValue from "./UseDebouncedValue";
+import UseDebouncedValue from "../utils/UseDebouncedValue.ts";
 import { useEffect } from "react";
 
 const SearchTerm = () => {
@@ -9,13 +9,18 @@ const SearchTerm = () => {
   const debouncedSearchTerm = UseDebouncedValue(searchTerm, 300);
 
   useEffect(() => {
-    setSearchTerm(debouncedSearchTerm)
-  }, [debouncedSearchTerm, setSearchTerm])
+    setSearchTerm(debouncedSearchTerm);
+  }, [debouncedSearchTerm, setSearchTerm]);
 
   return (
     <div className="relative sm:max-md:mb-[2rem] md:max-lg:w-[50%] lg:w-[40%]">
-      <Search className="absolute left-6 top-4 text-gray-500 dark:text-gray-300" />
-      <input type="search" className="sm:max-md:w-full py-4 pl-16 pr-3 w-3/4 dark:bg-[#2B3844] dark:text-gray-300 outline-none rounded-md" placeholder="Search for a country..." onChange={(e) => setSearchTerm(e.target.value)} />
+      <Search className="absolute left-6 top-4 text-gray-500" />
+      <input
+        type="search"
+        className="xsm:max-md:w-full py-4 pl-16 pr-3 w-3/4 dark:bg-[#2B3844] outline-none rounded-md"
+        placeholder="Search for a country..."
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
     </div>
   );
 };
